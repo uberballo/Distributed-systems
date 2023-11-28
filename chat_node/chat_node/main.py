370ba3e15bd3eb2b921c672712b97ec077cce7b0
Joakim Joensuu <joakim.joensuu@gmail.com>
2023-11-28 18:59:08 +0200

    Make code checkers happy
    Also place existing Python files into properly named modules so that they don't collide if installed in same environment.

--- chat_node/chat_node/main.py
+++ chat_node/chat_node/main.py
@@ -1,23 +1,26 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
 import httpx
+from fastapi import FastAPI
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.temp_data = ["hello","world"]
+async def lifespan(application: FastAPI):
+    application.state.temp_data = ["hello", "world"]
     yield
-    
+
 
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def read_root():
     return {"temp_data:": app.state.temp_data}
+
 
 @app.get("/main")
 async def get_main():
     async with httpx.AsyncClient() as client:
         response = await client.get("http://main-node:8000/main")
         res = response.json()
-    return {"Response from main: ":res}
+    return {"Response from main: ": res}
