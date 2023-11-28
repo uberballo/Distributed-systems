66410240b1d55cda1e06605509dd305d3b8d7794
Joakim Joensuu <joakim.joensuu@gmail.com>
2023-11-28 18:59:08 +0200

    Make code checkers happy
    Also place existing Python files into properly named modules so that they don't collide if installed in same environment.

--- main_node/main_node/main.py
+++ main_node/main_node/main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.temp_data = ["hello","world"]
+async def lifespan(application: FastAPI):
+    application.state.temp_data = ["hello", "world"]
     yield
-    
+
 
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def read_root():
     return {"temp_data:": app.state.temp_data}
 
+
 @app.get("/main")
-async def read_root():
+async def read_main():
     return {"Greetings from main node"}
